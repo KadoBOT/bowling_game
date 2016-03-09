5cf2878e27e095a0612d8ec07edcda64381d3c80
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2016-03-09 18:23:36 -0300

    strike bonus added

--- app.js
+++ app.js
@@ -6,21 +6,28 @@ var BowlingGame = (function () {
         this.pins = 10;
         this.pinsLeft = 10;
         this.pinsHit = 0;
+        this.total = [];
+        this.newRound = true;
+        this.bonusStrike = false;
     }
     BowlingGame.prototype.addRound = function () {
         this.addScore();
     };
     BowlingGame.prototype.countPins = function () {
+        var roundname = 'Frame ' + this.round;
+        document.getElementById('msg').querySelector('h2').innerHTML = roundname;
         this.pinsHit = Math.floor((Math.random() * this.pinsLeft) + 1);
+        var msg1 = 'First ball hit for: ' + this.pinsHit;
+        document.getElementById('msg').querySelector('h4').innerHTML = msg1;
         this.pinsLeft = this.pinsLeft - this.pinsHit + 1;
+        var msg2 = 'There are ' + (this.pinsLeft - 1) + ' pin(s) left';
+        document.getElementById('msg').querySelector('h6').innerHTML = msg2;
     };
     BowlingGame.prototype.addScore = function () {
-        if (this.pinsLeft == this.pins) {
+        if (this.newRound) {
             this.countPins();
         }
         var node = document.createElement("span");
-        if (this.round == this.round_max) {
-        }
         if (this.pinsHit != this.pins) {
             var second_score = Math.floor(Math.random() * this.pinsLeft);
             var firstnode = document.createTextNode(this.pinsHit.toString());
@@ -32,15 +39,37 @@ var BowlingGame = (function () {
                 else {
                     node.appendChild(secondnode);
                 }
+                var msg1 = 'Second ball hit: ' + second_score + ' pins';
+                document.getElementById('msg').querySelector('h4').innerHTML = msg1;
+                var msg2 = 'There are ' + (this.pinsLeft - second_score - 1) + ' pins left';
+                document.getElementById('msg').querySelector('h6').innerHTML = msg2;
                 document.getElementById("round-" + this.round.toString()).querySelector(".ball2").appendChild(node);
+                this.total.push(this.pinsHit + second_score);
+                if (this.bonusStrike) {
+                    var totalScore_1 = this.total[this.round - 1] + 10;
+                    this.total[this.round - 2] = totalScore_1 + this.total[this.round - 2];
+                    var total = this.total.reduce(function (a, b) {
+                        return a + b;
+                    });
+                    var update = total - this.total[this.round - 1];
+                    document.getElementById("round-" + (this.round - 1).toString()).querySelector('#total').innerHTML = update.toString();
+                    console.log(this.total);
+                    this.bonusStrike = false;
+                }
+                var totalScore = this.total.reduce(function (a, b) {
+                    return a + b;
+                });
+                document.getElementById("round-" + this.round.toString()).querySelector('#total').innerHTML = totalScore.toString();
                 this.roundBall = 1;
                 this.round++;
                 this.pinsLeft = this.pins;
+                this.newRound = true;
             }
             else {
                 node.appendChild(firstnode);
                 document.getElementById("round-" + this.round.toString()).querySelector(".ball1").appendChild(node);
                 this.roundBall++;
+                this.newRound = false;
             }
         }
         if (this.pinsHit == this.pins) {
@@ -48,8 +77,22 @@ var BowlingGame = (function () {
             node.appendChild(strike);
             document.getElementById("round-" + this.round.toString()).querySelector(".ball1").appendChild(node);
             document.getElementById("round-" + this.round.toString()).querySelector(".ball2").appendChild(document.createTextNode("-"));
+            this.total.push(this.pinsHit);
+            if (this.bonusStrike) {
+                var totalScore_2 = this.total[this.round - 1] + 10;
+                this.total[this.round - 2] = totalScore_2 + this.total[this.round - 2];
+                document.getElementById("round-" + (this.round - 1).toString()).querySelector('#total').innerHTML = this.total[this.round - 2].toString();
+                console.log(this.total);
+                this.bonusStrike = false;
+            }
+            var totalScore = this.total.reduce(function (a, b) {
+                return a + b;
+            });
+            document.getElementById("round-" + this.round.toString()).querySelector('#total').innerHTML = totalScore.toString();
+            this.bonusStrike = true;
             this.round++;
             this.pinsLeft = this.pins;
+            this.newRound = true;
         }
     };
     return BowlingGame;
--- app.ts
+++ app.ts
@@ -5,6 +5,9 @@ class BowlingGame{
   pins: number = 10;
   pinsLeft: number = 10;
   pinsHit: number = 0;
+  total: Array<number> = [];
+  newRound: boolean = true;
+  bonusStrike: boolean = false;
 
   constructor(){
   }
@@ -14,18 +17,21 @@ class BowlingGame{
   }
 
   countPins(){
+    const roundname = 'Frame ' + this.round;
+    document.getElementById('msg').querySelector('h2').innerHTML = roundname;
     this.pinsHit = Math.floor((Math.random() * this.pinsLeft) + 1);
+    const msg1 = 'First ball hit for: ' + this.pinsHit;
+    document.getElementById('msg').querySelector('h4').innerHTML = msg1;
     this.pinsLeft = this.pinsLeft - this.pinsHit + 1;
+    const msg2 = 'There are ' + (this.pinsLeft - 1) + ' pin(s) left';
+    document.getElementById('msg').querySelector('h6').innerHTML = msg2;
   }
 
   addScore(){
-    if(this.pinsLeft == this.pins){
+    if(this.newRound){
       this.countPins();
     }
     const node = document.createElement("span");
-    if(this.round == this.round_max){
-
-    }
     if(this.pinsHit != this.pins){
       const second_score = Math.floor(Math.random() * this.pinsLeft);
       const firstnode = document.createTextNode(this.pinsHit.toString());
@@ -33,18 +39,40 @@ class BowlingGame{
       if(this.roundBall%2==0){
         if(this.pinsHit + second_score == this.pins) {
           node.appendChild(document.createTextNode("/"));
+
         } else{
           node.appendChild(secondnode);
         }
+        const msg1 = 'Second ball hit: ' + second_score + ' pins';
+        document.getElementById('msg').querySelector('h4').innerHTML = msg1;
+        const msg2 = 'There are ' + (this.pinsLeft - second_score - 1) + ' pins left';
+        document.getElementById('msg').querySelector('h6').innerHTML = msg2;
         document.getElementById("round-" + this.round.toString()).querySelector(".ball2").appendChild(node);
+        this.total.push(this.pinsHit + second_score);
+        if(this.bonusStrike){
+          let totalScore = this.total[this.round - 1] + 10;
+          this.total[this.round - 2] = totalScore + this.total[this.round - 2];
+          let total = this.total.reduce((a, b) => {
+           return a + b;
+          });
+          let update = total - this.total[this.round -1];
+          document.getElementById("round-" + (this.round - 1).toString()).querySelector('#total').innerHTML = update.toString();
+          console.log(this.total)
+          this.bonusStrike = false;
+        }
+        let totalScore = this.total.reduce((a, b) => {
+          return a + b;
+        });
+        document.getElementById("round-" + this.round.toString()).querySelector('#total').innerHTML = totalScore.toString();
         this.roundBall = 1;
         this.round++;
         this.pinsLeft = this.pins;
+        this.newRound = true;
       } else {
         node.appendChild(firstnode);
         document.getElementById("round-" + this.round.toString()).querySelector(".ball1").appendChild(node);
         this.roundBall++;
-
+        this.newRound = false;
       }
     }
     if(this.pinsHit == this.pins){
@@ -52,8 +80,22 @@ class BowlingGame{
       node.appendChild(strike);
       document.getElementById("round-" + this.round.toString()).querySelector(".ball1").appendChild(node);
       document.getElementById("round-" + this.round.toString()).querySelector(".ball2").appendChild(document.createTextNode("-"));
+      this.total.push(this.pinsHit);
+      if(this.bonusStrike){
+        let totalScore = this.total[this.round - 1] + 10;
+        this.total[this.round - 2] = totalScore + this.total[this.round - 2];
+        document.getElementById("round-" + (this.round - 1).toString()).querySelector('#total').innerHTML = this.total[this.round-2].toString();
+        console.log(this.total)
+        this.bonusStrike = false;
+      }
+      let totalScore = this.total.reduce((a, b) => {
+        return a + b;
+      });
+      document.getElementById("round-" + this.round.toString()).querySelector('#total').innerHTML = totalScore.toString();
+      this.bonusStrike = true;
       this.round++;
       this.pinsLeft = this.pins;
+      this.newRound = true;
     }
 
   }
